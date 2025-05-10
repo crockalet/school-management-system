b2e26a25a8d79057cf62337e9722b054756e8374
crockalet <isse_1997@hotmail.com>
2025-05-10 17:04:20 +0500

    feat: basic structure

---
+++ app/Models/Classroom.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Classroom extends Model
+{
+    /** @use HasFactory<\Database\Factories\ClassroomFactory> */
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'subject',
+        'max_students',
+    ];
+
+    public function casts()
+    {
+        return [
+            'max_students' => 'integer',
+        ];
+    }
+
+    public function students()
+    {
+        return $this->hasMany(Student::class);
+    }
+
+}
